3303d970ced8d53889d30b91543d6034b735980b
Tudor <tjianu@liverpool.ac.uk>
2026-06-24 10:41:57 +0100

    remove video trimmer page

--- src/app/app/video-trimmer/page.tsx
+++
@@ -1,9 +0,0 @@
-import VideoTrimmer from "@/components/video-trimmer";
-
-export default function VideoTrimmerPage() {
-  return (
-    <div className="max-w-4xl mx-auto">
-      <VideoTrimmer />
-    </div>
-  );
-}
