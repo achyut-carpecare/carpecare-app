fed214592238271bc77b22718f93f0e418f1acea
Tudor <tjianu@liverpool.ac.uk>
2026-06-24 14:28:28 +0100

    fix pooling issues

--- src/features/database/index.ts
+++ src/features/database/index.ts
@@ -5,6 +5,11 @@ export * as schema from "./schema";
 
 config({ path: ".env" });
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, {
+  max: 10,
+  prepare: false,
+  idle_timeout: 20,
+  connect_timeout: 10,
+});
 export const db = drizzle({ client });
 export type DB = typeof db;
